384eac71a605e0e0ab04c930b75ee89f5d41da33
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-09-07 19:35:02 -0500

    Fix hash syntax

--- syntax_cleanup.rb
+++ syntax_cleanup.rb
@@ -11,11 +11,11 @@ person = {
 }
 
 post = {
-   image_src: "./images/beach.png",
-   caption: "At the beach with my besties",
-   timestamp: "4:37 PM August 13, 2019",
-   number_likes: 0,
-   comments: []
+  image_src: "./images/beach.png",
+  caption: "At the beach with my besties",
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: []
  }
 
 post2 = {
